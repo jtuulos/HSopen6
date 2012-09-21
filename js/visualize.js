cc23a766ef1c7d7b5ed9a8e5eceaf03c753e6171
Jyri Tuulos <jyri.tuulos@gmail.com>
2012-09-21 17:54:50 +0300

    add ODA amount to map, fix labels

--- js/visualize.js
+++ js/visualize.js
@@ -3,15 +3,22 @@ google.setOnLoadCallback(drawRegionsMap);
 
 function drawRegionsMap() {
     var data = _(countries).chain().map(function(country) {
-        var education_ratio = country.education_ratio["2009"];
+        var education_ratio = country.education_ratio["2009"],
+            oda_amount = 0;
+
+        if (_.isObject(country.oda_amount)) {
+            oda_amount = country.oda_amount["2009"];
+        }
+
         if (education_ratio > 0) {
             return [
-                country.name,
-                education_ratio
+                country.code,
+                Math.round(education_ratio),
+                oda_amount
             ];
         }
     }).compact().value();
-    data.unshift(["Country", "Education ratio"]);
+    data.unshift(["Country", "Girls per 100 boys", "ODA (million USD)"]);
 
     var dataTable = google.visualization.arrayToDataTable(data);
 
